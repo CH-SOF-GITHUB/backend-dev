a827d680800e324a9846f94f7912c344bd7b243c
CH-SOF-GITHUB <bchaker28@yahoo.com>
2024-12-10 12:34:25 +0100

    change response json pagination

--- models/categorie.js
+++ models/categorie.js
@@ -5,5 +5,5 @@ const categorieSchema = mongoose.Schema({
   nomcategorie: { type: String, required: true, unique: true },
   imagecategorie: { type: String, required: false }
 })
-
+//
 module.exports = mongoose.model('Categorie', categorieSchema)
--- routes/article.route.js
+++ routes/article.route.js
@@ -5,25 +5,31 @@ const Article = require('../models/article')
 const router = express.Router()
 
 // verify token for login
-const verifyToken = require('../routes/middleware/verify-token');
+const verifyToken = require('../routes/middleware/verify-token')
 
 // méthode POST
-router.post('/', async (req, res) =>  {  
-    const nouvarticle = new Article(req.body) 
-    try { 
-        const response =await nouvarticle.save(); 
-        const articles = await Article.findById(response._id).populate("scategorieID").exec(); 
-        res.status(200).json({articles, message: "Article enregistrée avec succès"}); 
-    } catch (error) { 
-        res.status(404).json({ message: error.message }); 
-    } 
-});
+router.post('/', async (req, res) => {
+  const nouvarticle = new Article(req.body)
+  try {
+    const response = await nouvarticle.save()
+    const articles = await Article.findById(response._id)
+      .populate('scategorieID')
+      .exec()
+    res
+      .status(200)
+      .json({ articles, message: 'Article enregistrée avec succès' })
+  } catch (error) {
+    res.status(404).json({ message: error.message })
+  }
+})
 
 // méthode GET: afficher la liste des articles
-router.get('/' ,async (req, res) => {
+router.get('/', async (req, res) => {
   // retour tableau
   try {
-    const articles = await Article.find({}, null, { sort: { '_id': -1 } }).populate("scategorieID");
+    const articles = await Article.find({}, null, {
+      sort: { _id: -1 }
+    }).populate('scategorieID')
     res.status(200).json(articles)
   } catch (error) {
     res.status(404).json({ message: error.message })
@@ -34,25 +40,45 @@ router.get('/' ,async (req, res) => {
 /* The `/art/pagination` route is used to implement pagination for retrieving a subset of
 articles based on the specified page number and page size. Here's a breakdown of what
 the route does: */
-router.get('/art/pagination', verifyToken ,async (req, res) => {
-  const filtre = req.query.filtre || "";
-  const page = parseInt(req.query.page);
-  const pageSize = parseInt(req.query.pageSize);
+router.get('/art/pagination', verifyToken, async (req, res) => {
+  const filtre = req.query.filtre || ''
+  const page = parseInt(req.query.page)
+  const pageSize = parseInt(req.query.pageSize)
 
   //calculate the start and end indexes for the requested page
-  const startIndex = (page - 1) * pageSize;
-  const endIndex = page * pageSize;
+  const startIndex = (page - 1) * pageSize
+  const endIndex = page * pageSize
 
-  const articles = await Article.find({designation:{$regex: filtre, $options: "i"}}, null, {sort: {'_id': -1}}).populate("scategorieID").exec()
+  const articles = await Article.find(
+    { designation: { $regex: filtre, $options: 'i' } },
+    null,
+    { sort: { _id: -1 } }
+  )
+    .populate('scategorieID')
+    .exec()
   //Slice the products array based on the indexes
 
-  const paginatedProducts = articles.slice(startIndex, endIndex);
+  const paginatedProducts = articles.slice(startIndex, endIndex)
 
   // calculate the number total of pages
-  const totalPages = Math.ceil(articles.length / pageSize);
+  const totalPages = Math.ceil(articles.length / pageSize)
 
   // Send the paginated products and total pages
-  res.json({ products: paginatedProducts, totalPages }); 
+  // res.json({ products: paginatedProducts, totalPages });
+  res.json({
+    success: true,
+    message: 'articles.details.success',
+    status: res.statusCode,
+    data: {
+      seo: paginatedProducts, // Les articles paginés
+      pagination: {
+        totalItems, // Nombre total d'articles
+        perPage: pageSize, // Nombre d'articles par page
+        page, // Page actuelle
+        totalPages // Nombre total de pages
+      }
+    }
+  })
 })
 
 // méthode DELETE
